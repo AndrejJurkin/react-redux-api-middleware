0a61a5368ef50ae313a01ba5e8ec1ca9f73cf295
Andrej Jurkin <andrej.jurkin@gmail.com>
2019-03-30 09:17:39 +0100

    Remove public folder

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const GettingStarted = () => <div>Hello!</div>;
-
-ReactDOM.render(<GettingStarted />, document.getElementById('root'));
